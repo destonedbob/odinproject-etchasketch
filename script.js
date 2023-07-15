5b7091d14c0a56f450d5f0a353521a12ac889235
Deston Chung <destonchung@gmail.com>
2023-07-15 11:22:57 +0800

    Add logic to change pixel color on hover

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ let gameMat = document.querySelector('.game-mat')
 let gameMatHeight = gameMat.clientHeight - parseFloat(getComputedStyle(gameMat).paddingLeft) * 2;
 const gridSize = 16;
 const GAME_PIXEL_CLASSNAME = "game-pixel"
+const ORIGINAL_GAME_PIXEL_COLOR = 'white'
+const HOVER_COLOR = 'grey'
 
+function changeColor() {
+    this.style.backgroundColor = HOVER_COLOR;
+}
+
+function resetColor() {
+    this.style.backgroundColor = ORIGINAL_GAME_PIXEL_COLOR;
+}
 
 function setUpGamePixels(N=16) {
     for (let i = 0; i < N*N; i++) {
         let div = document.createElement('div');
         div.classList.add(GAME_PIXEL_CLASSNAME)
+        div.addEventListener('mouseover', changeColor) // can be easily done with css class though
+        div.addEventListener('mouseout', resetColor)
         div.style.height = gameMatHeight / gridSize + 'px';
         div.style.width = gameMatHeight / gridSize + 'px';
         gameMat.appendChild(div);
